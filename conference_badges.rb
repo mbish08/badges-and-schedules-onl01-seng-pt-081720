a52c14eb72897c92909124830871f2c1f3607ad8
Mikel Bishop <mdrew8@gmail.com>
2020-08-27 01:57:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   rooms = []
 attendees.each do |name|
-  rooms << "Hello, #{name}! You'll be assigned to room #{counter}"
+  rooms << "Hello, #{name}! You'll be assigned to room #{counter}!"
   counter += 1 
 end
 rooms
