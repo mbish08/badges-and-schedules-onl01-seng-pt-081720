5c9773298a64dfcd62108b3c5b0bc470ed826f87
Mikel Bishop <mdrew8@gmail.com>
2020-08-27 00:48:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,9 @@ def badge_maker(name)
 end
 
 
-def batch_badge_creator(name)
-  batch_badge_creator.each do |name|
-  puts "Hello, my name is #{name}."
+def batch_badge_creator(attendees)
+  batch_badge_creator.each do |attendees|
+  puts "Hello, my name is #{attendees}."
 end
 end
 
